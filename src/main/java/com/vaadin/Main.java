650dee8ee8c8748a9aa2b89bf247711791a0201f
ClanEZ <bernardosoto30rus@gmail.com>
2025-05-29 18:42:29 +0400

    hw1

--- src/main/java/com/vaadin/Main.java
+++ src/main/java/com/vaadin/Main.java
@@ -1,7 +1,20 @@
 package com.vaadin;
 
+import static com.vaadin.hw1.twoArr;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Создаём два отсортированных массива
+        int[] arr = {-5, 0, 1, 1, 2, 3, 6, 8, 22, 45, 678};
+        int[] arr2 = {-10, 2, 3, 6, 7, 7, 7, 9, 11, 1000, 1010, 1100};
+
+        // Вызываем метод twoArr
+        int[] arr3 = twoArr(arr, arr2);
+
+        // Выводим результат
+        System.out.print("Результат объединения: ");
+        for (int num : arr3) {
+            System.out.print(num + " ");
+        }
     }
 }
